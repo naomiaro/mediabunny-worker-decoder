6366b72bb2f068843a1345c7c052b61298de59f3
Naomi Aro <naomiaro@gmail.com>
2025-07-21 17:06:41 -0700

    see streaming packets

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,4 @@
-import { Input, ALL_FORMATS, BufferSource } from 'mediabunny';
+import { Input, ALL_FORMATS, BufferSource, EncodedPacketSink } from 'mediabunny';
 
 self.onmessage = async (e) => {
   const buffer = e.data;
@@ -16,10 +16,36 @@ self.onmessage = async (e) => {
     const duration = await input.computeDuration(); // => 1905.4615
     console.log(duration)
 
-    const audio = await input.getPrimaryAudioTrack(); // => InputAudioTrack | null
+    const audioTrack = await input.getPrimaryAudioTrack(); // => InputAudioTrack | null
 
-    const canDecode = await audio.canDecode(); // => boolean
+    const canDecode = await audioTrack.canDecode(); // => boolean
     console.log(`Can decode ${canDecode}`)
+
+    // Get the number of audio channels:
+    console.log(`Number of channels ${audioTrack.numberOfChannels}`)
+
+    // Get the audio sample rate in hertz:
+    console.log(`sample rate ${audioTrack.sampleRate}`)
+
+    const decoderConfig = await audioTrack.getDecoderConfig(); // => AudioDecoderConfig | null
+
+    console.log(decoderConfig)
+
+    const sink = new EncodedPacketSink(audioTrack);
+
+    const decoder = new AudioDecoder({
+      output: console.log,
+      error: console.error,
+    });
+    decoder.configure(decoderConfig);
+
+    let currentPacket = await sink.getKeyPacket(0);
+    while (currentPacket && currentPacket.timestamp < duration) {
+      decoder.decode(currentPacket.toEncodedAudioChunk());
+      currentPacket = await sink.getNextPacket(currentPacket);
+    }
+
+    await decoder.flush();
 
 
     postMessage({duration});
